21b77588c9c5af0ccacba74d4c28ee688cc75925
libre <libre@localhost.localdomain>
2016-04-19 05:55:28 +0200

    removed utils/getSessionStorage

--- utils/getSessionStorage.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = function(){
-  if(typeof window !== 'undefined' && typeof window.sessionStorage !== 'undefined' && typeof window.sessionStorage.getItem !== 'undefined'){
-      return window.sessionStorage.getItem
-  }
-
-  return undefined
-}
